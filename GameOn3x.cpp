81de88c3e617a6b72dab8b7ffaf65659ae4beb8e
Victoria Olivarez <volivarez82457@uat.edu>
2026-01-15 13:33:40 -0700

    Add files via upload

---
+++ GameOn3x.cpp
@@ -0,0 +1,15 @@
+// GameOn3x.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+//Game Over 3.0
+//Demonstartes using Declarations
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+	cout << "Game on!" << endl;
+	return 0;
+}
